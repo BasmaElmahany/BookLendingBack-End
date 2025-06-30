b53e683d8eb0e32a23f726e86d0b4c3fa5f6627c
Basma Khalaf <basmakhalaf974@gmail.com>
2025-06-30 15:41:11 +0300

    Add files via upload
    ApplicationLayer

---
+++ BookLendingBackUp.Application/DTOs/CatalogDTO.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace BookLendingBackUp.Application.DTOs
+{
+    public class CatalogDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ BookLendingBackUp.Application/DTOs/DelayedBookDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLendingBackUp.Application.DTOs
+{
+    public class DelayedBookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public DateOnly PublishedAt { get; set; }
+        public string CatalogName { get; set; }
+        public string FullName { get; set; }
+        public DateTime BorrowedAt { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
