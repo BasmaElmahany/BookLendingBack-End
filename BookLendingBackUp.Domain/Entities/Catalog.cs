4ed31f1d0428a7faad6baf2b9fdcae553b3aa156
Basma Khalaf <basmakhalaf974@gmail.com>
2025-06-30 15:44:38 +0300

    Domain Layer
    Entities

---
+++ BookLendingBackUp.Domain/Entities/Catalog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLendingBackUp.Domain.Entities
+{
+    public class Catalog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Book>? Books { get; set; }   //allows null due to the catalog may be exisited and not has any books 
+
+    }
+}
